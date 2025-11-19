08858aa49a12d9fd0c0c0155a155f82fbbf43979
Chethuknchethan <chethankn507@gmail.com>
2025-11-19 03:01:55 +0000

    changesis done

---
+++ 01_basic/Datatypes.js
@@ -0,0 +1,21 @@
+"use strict" //treat all js code as newer vesion
+
+//datatypes
+let name="chethan"
+let age=23
+let present=true/false
+let maerried=null
+let older;
+
+//number
+//string
+//boolean
+//null
+//undefined
+//symbol
+//bigint
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined);
---
+++ 01_basic/Variables.js
@@ -0,0 +1,16 @@
+const accountId = 935328
+let accountMail = "chethu@gmail.com"
+var accountName= "chethan"
+accountLocation="bengaluru"
+
+// accountId=64545
+console.log(accountId);
+accountMail="chintu@gmail.com"
+var accountName="chinthu"
+
+//Note
+/*
+don't use var to declare variables why means its a older way of declaring
+becouse issue accurs in block and functional scope
+*/
+console.table([accountId,accountMail,accountName,accountLocation])
