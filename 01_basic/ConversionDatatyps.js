23ad21d27f83a11e9694c1e05605418546dc7662
Chethuknchethan <chethankn507@gmail.com>
2025-11-19 03:39:09 +0000

    conversion is done

---
+++ 01_basic/ConversionDatatyps.js
@@ -0,0 +1,35 @@
+let score=true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+// "45" =>45
+// "45abc" =>NaN (Not a number)
+//true =>1; false=>0
+//null => 0
+//undefdined => NaN
+
+let value =""
+console.log(typeof value);
+
+let booleanvalue=Boolean(value)
+console.log(typeof booleanvalue);
+console.log(booleanvalue);
+
+// 1 => true; 0=> flase
+//"" =>false; "chethan" => true
+//null => false
+//undefined => false 
+//NaN => false
+
+let nvalue=33
+let strvalue=String(nvalue)
+console.log(typeof strvalue);
+console.log(strvalue);
+
+
+
